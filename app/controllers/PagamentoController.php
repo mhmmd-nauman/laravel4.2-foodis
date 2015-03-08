d0017af8bc5da28bbb2d4d436a3cf08abeccd015
Cláudio Henrique <claudiohenriquedev@gmail.com>
2015-03-08 06:37:18 -0300

    iNFO: Inicio da Implementação de Pagamentos

---
+++ app/controllers/PagamentoController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudiohenrique
+ * Date: 08/03/15
+ * Time: 06:33
+ */
+
+class PagamentoController extends BaseController {
+
+    /* Rota para adicionar método de pagamento */
+    public function add(){
+        $input = Request::getContent();
+        $obj = json_decode($input);
+
+        $pagamento = new Pagamento();
+        $pagamento->tipo = $obj->tipo;
+        $pagamento->enabled = 1;
+        if($pagamento->save()){
+            echo json_encode(
+                array(
+                    "status" => 200,
+                    "message" => "Método de Pagamento adicionado com sucesso",
+                )
+            );
+        }
+    }
+
+
+}
---
+++ app/models/Pagamento.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: claudiohenrique
+ * Date: 08/03/15
+ * Time: 06:32
+ */
+
+
+class Pagamento extends Eloquent {
+    protected $table = 'pagamento';
+}
