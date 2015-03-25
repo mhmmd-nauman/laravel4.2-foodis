9a54cd9a1ac47cf67390c16450bcf23e2b1bae76
Cláudio Henrique <claudiohenriquedev@gmail.com>
2015-03-25 04:26:35 -0300

    iNFO: Recuperar Pedidos de um Usuário - 50%

--- app/controllers/PedidoController.php
+++ app/controllers/PedidoController.php
@@ -9,14 +9,44 @@
 class PedidoController extends BaseController {
 
     public function get($id_usuario){
-        $pedidos = Pedido::where('clientes_id','=',$id_usuario)->join('produto_pedido','produto_pedido.pedidos_id','=','pedidos.id')
-                                                               ->join('produtos','produtos.id','=','produto_pedido.produtos_id')->get()->toArray();
+        $informacoes_pedido = Pedido::where('clientes_id','=',$id_usuario)->join('produto_pedido','produto_pedido.pedidos_id','=','pedidos.id')
+                                                                ->join('produtos','produtos.id','=','produto_pedido.produtos_id')
+                                                                ->join('pagamento','pagamento.id','=','pedidos.pagamento_id')
+                                                                ->join('restaurantes','restaurantes.id','=','pedidos.restaurantes_id')
+                                                                ->get()->toArray();
+
+        $data = array();
+        $primeiro_id = $informacoes_pedido[0]['pedidos_id'];
 
 
-        $data = array_add();
+        for($i = 0; $i < sizeof($informacoes_pedido); $i++) {
+
+            /* Informações (id do Pedido, Numero do Pedido, Id do Restaurante, Tipo de Pagamento e Status */
+            $pedido = array(
+                "pedido_id" => $informacoes_pedido[$i]['pedidos_id'],
+                "numero_pedido" => $informacoes_pedido[$i]['numero_pedido'],
+                "restaurante_id" => $informacoes_pedido[$i]['nome_estabelecimento'],
+                "tipo_pagamento" => $informacoes_pedido[$i]['tipo'],
+                "valor_pedido" => $informacoes_pedido[$i]['valor_total'],
+                "status" => $informacoes_pedido[$i]['status']
+            );
+
+            /* Produtos de um Pedido Especifico */
+            foreach ($informacoes_pedido as $informacoes) {
+                $pedido[] = array(
+                    "nome_produto" => $informacoes['nome_produto'],
+                    "quantidade" => $informacoes['quantidade'],
+                    "preco" => $informacoes['preco'],
+                    "observacoes_produto_pedido" => $informacoes['observacoes_produto_pedido']
+                );
+            }
+
+        }
 
         echo '<pre>';
-        print_r($pedidos);
+        print_r($pedido);
+
+
     }
 }
 
