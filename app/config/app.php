9a08ba6dd69117ca13c4d8a27c5125062b3a5dc2
Cláudio Henrique <claudiohenriquedev@gmail.com>
2015-04-02 18:50:19 -0300

    Fix: removed nexmo package

--- app/config/app.php
+++ app/config/app.php
@@ -122,8 +122,6 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
         'Canducci\Cep\CepServiceProvider',
-        'Artistan\Nexmo\NexmoServiceProvider',
-
 	),
 
 	/*
